cb05ec277cc4efe0d464765eaba0739a26b31608
Astronaut701 <39848073+astronaut701@users.noreply.github.com>
2024-12-01 18:52:10 -0500

    Remove unused module

--- convert_wav.py
+++
@@ -1,7 +0,0 @@
-from pydub import AudioSegment
-
-if format=="mp3":
-    # convert mp3 to wav
-    audio = AudioSegment.from_mp3("input.mp3")
-    audio.export("output.wav", format="wav")
-    print('Audio converted to wav.')
